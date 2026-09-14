888232ea64a01c532a83895d5c5ed10f47051d2a
Nimesh <nimeshsitoula@gmail.com>
2026-09-14 21:23:17 +0545

    failing tests

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestGetApiKey(t *testing.T) {
-	Apikey := "ApiKey crazyapi6969" // Set the API key for testing
+	Apikey := "AppKey crazyapi6969" // Set the API key for testing
 
 	// 1. Create a new HTTP request
 	req, err := http.NewRequest("GET", "https://api.example.com/data", nil)
